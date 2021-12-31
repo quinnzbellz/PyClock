4600bbb7b465010776433f382c19d9b0990012af
Quinn Bellmore <quinnzbellz@tutanota.com>
2021-12-31 14:34:24 -0500

    centering things is stupid

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def clr():
     else:
         _ = system('clear')
 
-def print_centre(s):
-    tprint(s.center(shutil.get_terminal_size().columns))
+
 
 clr()
 time.sleep(0.4)
@@ -21,6 +20,6 @@ time.sleep(0.4)
 while True:
     tme = datetime.datetime.now()
     b = str(tme.strftime("%H:%M"))
-    print_centre(b)
+    tprint(b)
     time.sleep(1)
     clr()
