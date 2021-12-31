b51f205f8d58002d055efb31d65dee7c5c98bc1a
Quinn Bellmore <quinnzbellz@tutanota.com>
2021-12-30 22:08:35 -0500

    just screwing around lol

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+# Import libraries
 import datetime
 import time
 from os import system, name
 
+# Define a clear command to make clock update
 def clr():
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system('clear')
 
-
+# Starts an infinite loop of showing the time
 while True:
     tme = datetime.datetime.now()
     print(tme.strftime("%H:%M"))
